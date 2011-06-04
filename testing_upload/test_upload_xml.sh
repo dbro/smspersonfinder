a039797f568526f2df8da80139bdb7b30ef9555a
Lionel Young <lionelyoung@gmail.com>
2011-06-04 13:32:04 -0700

    Add example pfif 1.1 xml file for testing the upload

--- testing_upload/test_upload_xml.sh
+++ testing_upload/test_upload_xml.sh
@@ -1,2 +1,2 @@
-curl -X POST -H 'Content-type: application/xml' --data-binary @test_file.xml \
+curl -X POST -H 'Content-type: application/xml' --data-binary @pfif-1.1-example.xml \
     https://test-nokey.googlepersonfinder.appspot.com/api/write?key=auth_token
