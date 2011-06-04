b247e8050a4304aafe558880c13ba5d661251050
Lionel Young <lionelyoung@gmail.com>
2011-06-04 14:40:41 -0700

    Fix API for rhok1.com  The documentation was had the wrong API key

--- testing_upload/test_upload_xml.sh
+++ testing_upload/test_upload_xml.sh
@@ -1,2 +1,2 @@
 curl -X POST -H 'Content-type: application/xml' --data-binary @rhok-example.xml \
-    https://rhok.googlepersonfinder.appspot.com/api/write?key=punsOMMYMAI27tk
+    https://rhok.googlepersonfinder.appspot.com/api/write?key=punsOMMYMAI27tkr
