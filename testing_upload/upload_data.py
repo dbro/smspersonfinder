dda791ddbd389ff6c06a474f170a1eb3f64c87f7
Jonathan Koff <jonathankoff@gmail.com>
2011-06-04 14:43:47 -0700

    More functional PS write code, integrated with formatted message. Does not work yet.

--- testing_upload/upload_data.py
+++ testing_upload/upload_data.py
@@ -1,14 +1,45 @@
 #!/usr/bin/env python
 
-def build_url(action="write", domain="rhok1", key="punsOMMYMAI27tk"):
+import urllib2
+
+def build_url(action, domain, key):
     """
     Action is write/note/person
     """
     url = "https://%s.googlepersonfinder.appspot.com/api/%s?key=%s" % (domain, action, key)
     return url
 
+def build_request_body(formatted_msg, namespace):
+    # Formatted message format: Last#First#Comment
+    fields = formatted_msg.split('#')
+    return """
+    <?xml version="1.0" encoding="UTF-8"?> 
+    <pfif:pfif xmlns:pfif="http://zesty.ca/pfif/1.3"> 
+      <pfif:person> 
+        <pfif:person_record_id>%(namespace)s/person.113147</pfif:person_record_id> 
+        <pfif:first_name>%(first_name)s</pfif:first_name> 
+        <pfif:last_name>%(last_name)s</pfif:last_name> 
+        <pfif:other>description:
+%(comments)s
+        </pfif:other> 
+      </pfif:person> 
+    </pfif:pfif>
+    """ % {
+      'first_name': fields[1],
+      'last_name': fields[0],
+      'comments': fields[2],
+      'namespace': namespace
+    }
+
 if __name__ == "__main__":
-    domain = "rhok1"
-    key = "punsOMMYMAI27tk"
+    domain = "rhok"
+    key = "punsOMMYMAI27tkr"
     action = "write"
-    print build_url("write")
+    namespace = "rhok1.com"
+
+    url = build_url(action, domain, key)
+    print url
+
+    data = build_request_body('Koff#Jonathan#No comments.', namespace)
+    req = urllib2.Request(url, data)
+    print urllib2.urlopen(req).read()
