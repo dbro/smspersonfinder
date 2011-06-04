09b832b382dd5adc1604381cfadeabfdeb9d6472
Lionel Young <lionelyoung@gmail.com>
2011-06-04 14:02:33 -0700

    Add stub for building url to upload_data.py

--- testing_upload/upload_data.py
+++ testing_upload/upload_data.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
 
+def build_url(action="write", domain="rhok1", key="punsOMMYMAI27tk"):
+    """
+    Action is write/note/person
+    """
+    url = "https://%s.googlepersonfinder.appspot.com/api/%s?key=%s" % (domain, action, key)
+    return url
+
 if __name__ == "__main__":
     domain = "rhok1"
     key = "punsOMMYMAI27tk"
-    url = "https://%s.googlepersonfinder.appspot.com/api/write?key=%s" % (domain, key)
-    print url
+    action = "write"
+    print build_url("write")
