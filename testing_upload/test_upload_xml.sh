e9d97efce91038a9d1f76910e889f055b438472f
Lionel Young <lionelyoung@gmail.com>
2011-06-04 13:27:26 -0700

    Add testing_upload stubs for Upload part

---
+++ testing_upload/test_upload_xml.sh
@@ -0,0 +1,2 @@
+curl -X POST -H 'Content-type: application/xml' --data-binary @test_file.xml \
+    https://test-nokey.googlepersonfinder.appspot.com/api/write?key=auth_token
---
+++ testing_upload/upload_data.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    domain = "test-nokey"
+    url = "https://%s.googlepersonfinder.appspot.com/api/write" % domain
+    print url
