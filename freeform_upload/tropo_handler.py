da1b6479005cb0339dfbfb1cdee75cbb8a172826
Lionel Young <lionelyoung@gmail.com>
2011-06-04 19:37:26 -0700

    Add Tropo handler for SMS

---
+++ freeform_upload/tropo_handler.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import urllib
+import datetime
+
+TROPO = False
+
+# Set up payload
+time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+source = currentCall.callerID if TROPO else 'debug source'
+message = currentCall.initialText if TROPO else 'debug message'
+
+# Build query string
+q = {'source': source,
+    'message': message,
+    'time': time}
+
+query = urllib.urlencode(q)
+
+# Deliver payload
+url="http://smspersonfinder.appspot.com/create?%s" % query
+if not TROPO:
+    print "Opening %s" % url
+resp = urllib.urlopen(url)
+html = resp.read()
+
+# Output success/failure
+if html:
+    print "Success"
+    print html
+    if TROPO:
+        say('Successfully sent')
+else:
+    print "Failure"
+    if TROPO:
+        say('Failed send, please send again.')
